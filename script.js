6620dff3c820ae130290a61da16cbffa1220276d
dogdrips3 <dogdrips3444@gmail.com>
2025-09-02 18:48:12 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Script simples para o formulário
+document.querySelector("form").addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert("Mensagem enviada com sucesso!");
+  this.reset();
+});
